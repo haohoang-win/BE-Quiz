75ba56bb4de1af5b70250550591387c89608532d
Hao Hoang <hoo.hoang.hao@gmail.com>
2023-02-06 14:55:56 +0700

    env production and convert stringify

--- src/controllers/loginRegisterController.js
+++ src/controllers/loginRegisterController.js
@@ -42,9 +42,9 @@ const handleLogin = async (req, res) => {
         // set cookie
         if (data && data.DT && data.DT.access_token) {
             if (process.env.NODE_ENV === 'production') {
-                res.cookie('jwt', data.DT.access_token, { httpOnly: true, maxAge: 2 * 60 * 60 * 1000 })
+                res.cookie('jwt', JSON.stringify(data.DT.access_token), { httpOnly: true, maxAge: 2 * 60 * 60 * 1000 })
             } else {
-                res.cookie('jwt', data.DT.access_token, { httpOnly: true, maxAge: 2 * 60 * 60 * 1000 })
+                res.cookie('jwt', JSON.stringify(data.DT.access_token), { httpOnly: true, maxAge: 2 * 60 * 60 * 1000 })
             }
         }
         return res.status(200).json({
